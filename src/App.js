6e0a8f46ced50205f8ca61e6a000ea30982b2527
Per Kristian Svanberg <kristiansvanberg@gmail.com>
2020-09-03 20:33:36 +0200

    Finished building components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import Header from './components/Header';
+import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+const App = () => {
+	return (
+		<>
+			<Header />
+			<div className="game-container">
+				<Figure />
+				<WrongLetters />
+				<Word />
+			</div>
+		</>
+	)
 }
 
-export default App;
+export default App
+
